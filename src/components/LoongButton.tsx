cf39fdc5e6c240cc906d48d197b51d932bcbf06d
lrczcm <2508606977@qq.com>
2025-06-08 00:14:10 +0800

    无敌的按钮

--- src/components/LoongButton.tsx
+++ src/components/LoongButton.tsx
@@ -4,19 +4,28 @@ export const LoongButton = (props: { onClick?: MouseEventHandler<HTMLButtonEleme
     return (
         <button
             onClick={props.onClick}
-            className={`flex items-center gap-1 text-sm
-            bg-${props.level ?? "accent"}-4
-            border-b-4 border-${props.level ?? "accent"}-5
-            px-3 py-1.5 rounded-lg
-            transition-all duration-75 ease-in-out
-            hover:shadow-md active:shadow-sm
-            active:translate-y-[2px]
-            ${props.className ?? ''}
-          `}
+            className={`relative flex items-center gap-1 text-sm
+    bg-${props.level ?? "accent"}-4
+    px-3 py-1.5 rounded-lg border-b-4 border-${props.level ?? "accent"}-5
+    transition-all duration-75 ease-in-out 
+    hover:shadow-[0_4px_6px_rgba(255,219,220,0.5)] active:shadow-sm
+    active:translate-y-[3px]
+    hover:border-b-4 
+    hover:border-accent-4
+    before:content-[''] before:absolute before:inset-x-0 before:bottom-0
+    before:h-1 before:rounded-b-lg
+    before:bg-${props.level ?? "accent"}-500
+    ${props.className ?? ''}
+  `}
         >
-            {props.children}
+  <span className="relative top-[2px] flex items-center gap-1">
+    {props.children}
+  </span>
         </button>
 
 
+
     )
+
+
 }
